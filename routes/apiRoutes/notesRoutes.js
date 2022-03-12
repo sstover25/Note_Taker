4dffbdc35370ae651b42456e38392b50c3d6542f
SSto <95592688+sstover25@users.noreply.github.com>
2022-03-12 17:02:12 -0600

    finalized POST functionality

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -4,10 +4,14 @@ const { nanoid } = require("nanoid");
 const fs = require("fs");
 const path = require("path");
 
+// when a GET request is made, the notes db sends it's information
 router.get("/notes", (req, res) => {
   res.json(notes);
 });
 
+// when a POST request happens, a new random id is assigned to the body
+// the body of the request is then pushed into the notes array
+// the notes db is updated with the new object via fs.writeFileSync
 router.post("/notes", (req, res) => {
   req.body.id = nanoid();
   notes.push(req.body);
