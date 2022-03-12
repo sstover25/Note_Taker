a909b752d7cf28f9d8317072bfd5528fdca585cb
SSto <95592688+sstover25@users.noreply.github.com>
2022-03-12 13:55:59 -0600

    set up server.js file

--- server.js
+++ server.js
@@ -1,3 +1,21 @@
 const express = require("express");
+const { notes } = require("./db/db.json");
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
 
 const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+
+app.use("/api", apiRoutes);
+
+app.use("/", htmlRoutes);
+
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
+});
