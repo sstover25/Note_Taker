9b4589f0a05c5ced314aae74764b51aa99b1dac5
SSto <95592688+sstover25@users.noreply.github.com>
2022-03-12 16:52:15 -0600

    added unique ids and confirmed POST adds new note to db

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const { notes } = require("../../db/db.json");
 const { nanoid } = require("nanoid");
+const fs = require("fs");
+const path = require("path");
 
 router.get("/notes", (req, res) => {
   res.json(notes);
@@ -8,7 +10,12 @@ router.get("/notes", (req, res) => {
 
 router.post("/notes", (req, res) => {
   req.body.id = nanoid();
-  res.send(req.body);
+  notes.push(req.body);
+  fs.writeFileSync(
+    path.join(__dirname, "../../db/db.json"),
+    JSON.stringify({ notes: notes }, null, 2)
+  );
+  res.json(notes);
 });
 
 module.exports = router;
